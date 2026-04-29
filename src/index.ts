ba164e6ebf86d245a9e90ab6c481d13486586d96
jlenon7 <lenonSec7@gmail.com>
2026-04-29 12:12:12 -0300

    chore: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -25,9 +25,15 @@ export { OtelProvider } from '#src/providers/OtelProvider'
 export { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
 export { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
 export { getNodeAutoInstrumentations } from '#src/helpers/getNodeAutoInstrumentations'
-export { OTLPLogExporter as HttpOTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
+
 export { OTLPLogExporter as GrpcOTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-grpc'
-export { OTLPTraceExporter as HttpOTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+export { OTLPLogExporter as HttpOTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
+export { OTLPLogExporter as ProtoOTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto'
+
 export { OTLPTraceExporter as GrpcOTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
-export { OTLPMetricExporter as HttpOTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http'
+export { OTLPTraceExporter as HttpOTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+export { OTLPTraceExporter as ProtoOTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
+
 export { OTLPMetricExporter as GrpcOTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc'
+export { OTLPMetricExporter as HttpOTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http'
+export { OTLPMetricExporter as ProtoOTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto'
